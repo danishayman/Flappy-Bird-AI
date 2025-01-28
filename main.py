f4bc7cba3ee40b2070e2781d777571814da7c98f
ayman <danishaiman3b@gmail.com>
2025-01-29 04:15:29 +0800

    Add Base class and integrate into main game loop for Flappy Bird

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import pygame
-from variables import WIN_WIDTH, WIN_HEIGHT, BIRD_IMGS, PIPE_IMG, BASE_IMG, BG_IMG
+
 from bird import Bird
+from pipe import Pipe
+from base import Base
+from variables import WIN_WIDTH, WIN_HEIGHT, BIRD_IMGS, PIPE_IMG, BASE_IMG, BG_IMG
 
 
-def draw_window(win, bird):
+
+
+
+def draw_window(win, bird, pipes, base):
     win.blit(BG_IMG, (0, 0))
+    
+    for pipe in pipes:
+        pipe.draw(win)
+        
+    base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
+
+
+
+
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
     
@@ -19,8 +36,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        bird.move()
-        draw_window(win, bird)
+        # bird.move()
+        base.move()
+        draw_window(win, bird, pipes, base)
         
                 
                 
